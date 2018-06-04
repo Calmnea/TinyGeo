37df047dbc296578ab7d06c95234dc9d6ec7be20
calmnea <calmnea@gmail.com>
2018-06-05 01:11:05 +0800

    add: draw straight function

--- tinygeo/display/geoview.py
+++ tinygeo/display/geoview.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 from matplotlib.lines import Line2D
 from copy import deepcopy
 from tinygeo import Point, Segment, Straight, Line, Polygon, MBR, GetMBR
+from tinygeo.constant import isZero, MATH
 
 
 class GeoViewer(object):
@@ -13,6 +14,11 @@ class GeoViewer(object):
         self.plt = plt
         self.figure, self.ax = self.plt.subplots()
         self._mbr = None
+        self._buffer = 1
+        self._straights = []
+
+    def set_buffer(self, buffer):
+        self._buffer = buffer
 
     def _update_mbr(self, geo):
         mbr = GetMBR(geo)
@@ -32,15 +38,26 @@ class GeoViewer(object):
             Line2D(xs, ys, color=color, linewidth=linewidth, linestyle=style))
         self._update_mbr(sg)
 
-    def _draw_straight(self, st, color, linewidth, style):
-        raise NotImplementedError
-
     def _draw_line(self, l, color, linewidth, style):
         xs = [p.x for p in l.points]
         ys = [p.y for p in l.points]
         self.ax.add_line(
             Line2D(xs, ys, color=color, linewidth=linewidth, linestyle=style))
         self._update_mbr(l)
+
+    def _add_straight(self, st, color, linewidth, style):
+        self._straights.append((st, color, linewidth, style))
+
+    def _draw_straight(self, st, color, linewidth, style):
+        if not self._mbr:
+            self._mbr = MBR(Point(-1, -1), Point(1, 1))
+        if isZero(st.B):
+            _segment = st.toSegment(self._mbr.top() + self._buffer,
+                                    self._mbr.bottom() - self._buffer)
+        else:
+            _segment = st.toSegment(self._mbr.left() - self._buffer,
+                                    self._mbr.right() + self._buffer)
+        self._draw_segment(_segment, color, linewidth, style)
 
     def _draw_polygon(self, poly, color, linewidth, style):
         xs = [p.x for p in poly.points + [poly.points[0]]]
@@ -57,15 +74,19 @@ class GeoViewer(object):
         elif isinstance(geo, Line):
             self._draw_line(geo, color, linewidth, style)
         elif isinstance(geo, Straight):
-            self._draw_straight(geo, color, linewidth, style)
+            self._add_straight(geo, color, linewidth, style)
         elif isinstance(geo, Polygon):
             self._draw_polygon(geo, color, linewidth, style)
         else:
             raise TypeError
 
-    def show(self, buffer=1):
+    def show(self):
         self.ax.set_xlim(
-            left=self._mbr.left() - buffer, right=self._mbr.right() + buffer)
+            left=self._mbr.left() - self._buffer,
+            right=self._mbr.right() + self._buffer)
         self.ax.set_ylim(
-            bottom=self._mbr.bottom() - buffer, top=self._mbr.top() + buffer)
+            bottom=self._mbr.bottom() - self._buffer,
+            top=self._mbr.top() + self._buffer)
+        for st in self._straights:
+            self._draw_straight(*st)
         self.plt.show()
